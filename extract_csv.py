cda83080c779ff2405faf6246678288daada5695
Steven Holroyd <stevenmobile98@gmail.com>
2022-03-10 09:11:14 +0000

    Commit working code files

---
+++ extract_csv.py
@@ -0,0 +1,44 @@
+import csv
+from datetime import datetime
+def extract_csv():
+    products_price_list=[]
+    products_dict_list=[]
+    
+    with open('Chesterfield.csv', 'r', newline='') as file:
+        extracted_list=[]
+        header=['Date', 'Location', 'name', 'product', 'total','payment_method', 'card_number']
+        reader=csv.DictReader(file,header)
+        for row in reader:
+    
+            #Split date/time
+            transaction_date_time = row['Date'].split(' ')
+            date = {'Date': transaction_date_time[0]}
+            time= {'Time': transaction_date_time[1]}
+        
+            new_date=datetime.strptime(transaction_date_time[0],'%d/%m/%Y').strftime('%m/%d/%Y')
+            date = {'Date': new_date}
+
+            row.update(date)
+            row.update(time)
+            
+            if row['payment_method']=='CASH':
+                row.pop('card_number')
+            if '' not in row.values():
+    
+                products_split=row['product'].split(',')
+                for each_product in products_split:
+                    product_price=each_product[-4:]
+                    product=each_product[:len(each_product)-7]
+                    product = product.strip()
+                    product_price=product_price.strip()
+                    single_product_dict={'product': product,'price': product_price}
+                    products_price_list.append(single_product_dict)
+                    single_product_dict={}
+
+                row.pop('name')
+                if 'card_number' in row:
+                    row.pop('card_number')
+                row['products']=products_price_list
+                extracted_list.append(row)
+                products_price_list=[]
+    return extracted_list
